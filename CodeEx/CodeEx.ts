f7a46c4347c6431e88a9178450cf103f5b914830
kuehar <green95251@gmail.com>
2022-03-03 18:55:49 +0900

    added P.18 Ex2.2.1.2 code to CodeEx.ts.

--- CodeEx/CodeEx.ts
+++ CodeEx/CodeEx.ts
@@ -1,5 +1,5 @@
 // P.18 2.2.1.1
-
+/*
 var foo = 0;
 
 function func(){
@@ -10,4 +10,18 @@ function func(){
   console.log(foobar);
 }
 func();
+*/
 
+// P.18 2.2.1.2
+let bar = 1;
+const baz = 2;
+
+function func(){
+  if(true){
+    let foobar1 = 'foobar1';
+    const foobar2 = 'foobar2';
+  }
+  // スコープ外なので出力エラー
+  console.log(foobar1); 
+  console.log(foobar2);
+}
