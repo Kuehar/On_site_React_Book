eac42609a32f70e07c8d3b78bb4d6c25251c0ad2
kuehar <green95251@gmail.com>
2022-03-05 11:12:40 +0900

    fixed App.tsx to P.44 2.4.1.5 codes.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,29 +1,10 @@
+// P.44 2.4.1.5
+
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Hello from './hello';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <div className="App-title">
-           Welcome to my app!!
-        </div>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+ReactDOM.render(
+  <Hello />,
+  document.getElementById('root')
+);
