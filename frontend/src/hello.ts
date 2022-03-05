75390116bec2af9793140a5a4e6e1604e314b4e3
kuehar <green95251@gmail.com>
2022-03-05 11:13:15 +0900

    added hello.ts to src folder.

---
+++ frontend/src/hello.ts
@@ -0,0 +1,11 @@
+// P.44 2.4.1.6
+
+import React ,{ Component } from 'react';
+
+class Hello extends Component{
+    render(){
+        return <div>Hello,world!!</div>;
+    }
+}
+
+export default Hello;
